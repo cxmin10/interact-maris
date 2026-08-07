a1ea53ed86873b43de21dda65214862555336ca3
cxmin <cxmin10@gmail.comm>
2026-08-07 10:56:23 +0300

    Fix Render frontend paths

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,8 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/interact-maris/",
-
   plugins: [
     react(),
     tailwindcss(),
